0d91501deef1e8f8a8aae5c9f75dffb87162d9c3
M-Bom <150365129+M-Bom@users.noreply.github.com>
2025-03-24 13:42:25 -0600

    Upadted to use javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.macewan.cmpt305.groupproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.macewan.cmpt305.groupproject to javafx.fxml;
+    exports ca.macewan.cmpt305.groupproject;
+}
