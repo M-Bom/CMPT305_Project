11a5fca0d93302c6030477e788604d6e03de30a3
M-Bom <150365129+M-Bom@users.noreply.github.com>
2025-03-26 20:50:29 -0600

    Added working filters that change and update table with search and reset buttons (price filter currently is not working will need to update to have it work).

--- src/main/java/ca/macewan/cmpt305/groupproject/HelloApplication.java
+++ src/main/java/ca/macewan/cmpt305/groupproject/HelloApplication.java
@@ -3,20 +3,25 @@ package ca.macewan.cmpt305.groupproject;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.List;
 
 
@@ -29,18 +34,26 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 1280, 960);
+
+        String csvFileName = "src/main/resources/Property_Assessment_Data_2024.csv";
+        PropertyAssessments propertyAssessments = new PropertyAssessments(csvFileName);
+        PropertyAssessments residentialFilteredPropertyAssessments = propertyAssessments.getFilteredData("Residential");
+
         BorderPane bp = new BorderPane();
         bp.setPadding(new Insets(10,10,10,10));
 
         Scene scene = new Scene(bp, 1800, 960);//new Scene(fxmlLoader.load(), win_width, win_height);
-        stage.setTitle("Employee Example");
+        stage.setTitle("Map search");
 
         VBox vb1 = new VBox();
 
-        vb1 = setVB1(vb1);
+        vb1 = setVB1(vb1, residentialFilteredPropertyAssessments);
 
         bp.setLeft(vb1);
 
+        onSearch(vb1, residentialFilteredPropertyAssessments);
+
+        onReset(vb1, residentialFilteredPropertyAssessments);
 
         //VBox layout = new VBox(10);
         //layout.setPadding(new Insets(20,20,20,20));
@@ -51,13 +64,11 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    VBox setVB1(VBox vb) throws IOException {
-        String csvFileName = "src/main/resources/Property_Assessment_Data_2024.csv";
-        PropertyAssessments propertyAssessments = new PropertyAssessments(csvFileName);
+    VBox setVB1(VBox vb, PropertyAssessments propertyAssessments) throws IOException {
         List<Neighbourhood> neighbourhoods = propertyAssessments.getAllNeighbourhoods();
         List<Address> addresses = propertyAssessments.getAllAddresses();
 
-        vb.setPrefWidth(700);
+        //vb.setPrefWidth(700);
 
         table = new TableView();
         data = FXCollections.observableArrayList(propertyAssessments.getData());
@@ -91,9 +102,74 @@ public class HelloApplication extends Application {
         ChoiceBox<String> priceFilter = new ChoiceBox<>();
         priceFilter.getItems().addAll("0 - 99,999","100,000 - 499,999", "500,000 - 999,999", "1,000,000+");
 
-        vb.getChildren().addAll(neighbourhoodFilter, priceFilter, table);
+        //create search and reset button
+        Button search = new Button("Search");
+        Button reset = new Button("Reset");
+        HBox hb = new HBox(10);
+
+        hb.getChildren().addAll(neighbourhoodFilter, priceFilter, search, reset);
+
+        vb.getChildren().addAll(hb, table);
 
         return vb;
+    }
+
+    void onSearch(VBox vb, PropertyAssessments propertyAssessments) {
+        // retrieve neighbourhoodFilter from vertical box
+        ChoiceBox<Neighbourhood> neighbourhoodFilter = (ChoiceBox<Neighbourhood>) ((HBox) vb.getChildren().getFirst()).getChildren().get(0);
+
+        // retrieve priceFilter from vertical box
+        ChoiceBox<String> priceFilter = (ChoiceBox<String>) ((HBox) vb.getChildren().getFirst()).getChildren().get(1);
+
+        // retrieve search button from vertical box
+        Button search = (Button) ((HBox) vb.getChildren().getFirst()).getChildren().get(2);
+
+        // Event handler for search button on click
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Neighbourhood neighbourhood = neighbourhoodFilter.getValue();
+                String filter1 = neighbourhood.getNeighbourhoodName();
+                System.out.println(filter1);
+                String filter2 = priceFilter.getValue();
+                System.out.println(filter2);
+                if (filter1 != null && filter2 == null) {
+                    PropertyAssessments filteredPropertyAssessments = propertyAssessments.getFilteredData(filter1);
+                    //PropertyAssessments residentialFilteredPropertyAssessments = filteredPropertyAssessments.getFilteredData("Residential");
+                    table.getItems().clear();
+                    table.getItems().addAll(filteredPropertyAssessments.getData());
+                } else if (filter1 != null) {
+                    PropertyAssessments filteredPropertyAssessments1 = propertyAssessments.getFilteredData(filter1);
+                    //PropertyAssessments residentialFilteredPropertyAssessments = filteredPropertyAssessments1.getFilteredData("Residential");
+                    table.getItems().clear();
+                    table.getItems().addAll(filteredPropertyAssessments1.getData());
+                }
+            }
+        };
+        search.setOnAction(event);
+    }
+
+    void onReset(VBox vb, PropertyAssessments propertyAssessments) {
+        // retrieve neighbourhoodFilter from vertical box
+        ChoiceBox<Neighbourhood> neighbourhoodFilter = (ChoiceBox<Neighbourhood>) ((HBox) vb.getChildren().getFirst()).getChildren().get(0);
+
+        // retrieve priceFilter from vertical box
+        ChoiceBox<String> priceFilter = (ChoiceBox<String>) ((HBox) vb.getChildren().getFirst()).getChildren().get(1);
+
+        // retrieve search button from vertical box
+        Button reset = (Button) ((HBox) vb.getChildren().getFirst()).getChildren().get(3);
+
+        // Event handler for search button on click
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                table.getItems().clear();
+                table.setItems(FXCollections.observableArrayList(propertyAssessments.getData()));
+                neighbourhoodFilter.getSelectionModel().clearSelection();
+                priceFilter.getSelectionModel().clearSelection();
+            }
+        };
+        reset.setOnAction(event);
     }
 
     public static void main(String[] args) {
